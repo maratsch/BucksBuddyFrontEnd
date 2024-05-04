b88f725320666b94693025399ae3c23225a08fa2
maratsch <s0589160@htw-berlin.de>
2024-05-04 11:38:58 +0200

    Added dropdown menu for currency in home and added logic to see the different currencys in history

--- src/components/ChooseCurrency.vue
+++ src/components/ChooseCurrency.vue
@@ -6,7 +6,7 @@
   <div class="card">
     <div class="card-body">
   <label for="dateInput" class="form-label">In which currency do you want to convert your expenses?</label>
-  <select class="form-select" id="currencySelect" v-model="currency">
+  <select class="form-select" id="currencySelect" v-model="currency"> <!-- TODO: ADD connection to database -->
     <option value="EUR">EUR</option>
     <option value="USD">USD</option>
     <option value="JPY">JPY</option>
