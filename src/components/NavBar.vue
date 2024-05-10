423b2f4ddb850110bebfc0fefd49bc99968e1cb9
Norman <peters.norman91@gmail.com>
2024-05-10 17:27:45 +0200

    install bootstrap icons

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,7 +7,7 @@
   <nav class="navbar shadow mb-3 navbar-expand-lg bg-body-tertiary">
     <div class="container">
       <div class="col d-flex align-items-center">
-        <RouterLink to="/"><img src="@/assets/logo2.png" alt="Bootstrap" style="width: auto; height: 70px;"></RouterLink>
+        <RouterLink to="/main"><img src="@/assets/logo2.png" alt="Bootstrap" style="width: auto; height: 70px;"></RouterLink>
       </div>
       <ul class="navbar-nav ms-auto align-self-center">
         <li class="nav-item">
