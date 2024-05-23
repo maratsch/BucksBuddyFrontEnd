b23a82bf9b609b2becba9c1b81e7806117f92925
maratsch <s0589160@htw-berlin.de>
2024-05-23 15:32:52 +0200

    changed address

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: '3.125.183.140/api',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
