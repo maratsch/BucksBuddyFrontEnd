0f23144dc6afc34357e1ce8e87863b440d9bf123
maratsch <s0589160@htw-berlin.de>
2024-05-29 12:35:52 +0200

    changed router Link

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './assets/styles.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index.ts'
 import { createPinia } from 'pinia'
 
 createApp(App).use(createPinia()).use(router).mount("#app")
