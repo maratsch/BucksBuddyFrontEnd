9e2a969168eeaa07d5cd1c6daa0ba5bf57d61b0f
maratsch <s0589160@htw-berlin.de>
2024-05-29 12:41:00 +0200

    removed the .ts endings

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import './assets/styles.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router/index.ts'
+import router from './router'
 import { createPinia } from 'pinia'
 
 createApp(App).use(createPinia()).use(router).mount("#app")
 
-import { useThemeStore } from './stores/themeStore.ts';
+import { useThemeStore } from './stores/themeStore';
 const themeStore = useThemeStore();
 themeStore.loadTheme();
