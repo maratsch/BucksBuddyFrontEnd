3c8ba95c22b343bd63c30f8800ac63e52581ba25
Norman <peters.norman91@gmail.com>
2024-06-12 14:09:59 +0200

    usercard

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -33,6 +33,7 @@
       <hr>
       <div class="text-center text-danger">
         <h5>Danger Zone</h5>
+        <p>Deleting your account is permanent and cannot be undone.</p>
       </div>
       <div class="text-center mb-3">
         <button @click="deleteUser" class="btn btn-danger custom-width-btn">Delete User</button>
