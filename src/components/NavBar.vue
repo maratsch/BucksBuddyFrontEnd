1fce3f52156c0cd04ee6ee4dc2c0677ac8f11581
Norman <peters.norman91@gmail.com>
2024-06-22 13:02:52 +0200

    dark mode adjustments

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,4 +1,4 @@
-<!--src/components/NavBar.vue-->
+<!-- src/components/NavBar.vue -->
 
 <script setup lang="ts">
 import {useThemeStore} from '@/stores/themeStore';
@@ -6,14 +6,15 @@ import {useRouter} from "vue-router";
 
 const themeStore = useThemeStore();
 const toggleTheme = () => {
-  themeStore.applyTheme(themeStore.theme === 'light' ? 'dark' : 'light');}
+  themeStore.applyTheme(themeStore.theme === 'light' ? 'dark' : 'light');
+}
 
-  const router = useRouter();
-  const logout = () => {
-    localStorage.removeItem('UUID')
-    localStorage.removeItem('selectedJourney')
-    router.push('/login');
-  }
+const router = useRouter();
+const logout = () => {
+  localStorage.removeItem('UUID')
+  localStorage.removeItem('selectedJourney')
+  router.push('/login');
+}
 </script>
 
 <template>
@@ -24,35 +25,35 @@ const toggleTheme = () => {
           <img src="@/assets/logo2.png" alt="Bootstrap" style="width: auto; height: 70px;">
         </RouterLink>
       </div>
-      <div class="col-10 d-flex justify-content-ends">
-        <ul class="navbar-nav ms-auto ">
+      <div class="col-10 d-flex justify-content-end">
+        <ul class="navbar-nav ms-auto">
           <li class="nav-item">
             <RouterLink to="/user"
-                        class="bi bi-person-circle m-2 fs-3 icon-link-hover"
+                        :class="['bi bi-person-circle m-2 fs-3 icon-link-hover', themeStore.theme === 'light' ? 'text-dark' : 'text-white']"
                         title="User">
             </RouterLink>
           </li>
           <li class="nav-item">
             <RouterLink to="/newjourney"
-                        class="bi bi-plus-lg m-2 fs-3 icon-link-hover"
+                        :class="['bi bi-plus-lg m-2 fs-3 icon-link-hover', themeStore.theme === 'light' ? 'text-dark' : 'text-white']"
                         title="Create a new Journey">
             </RouterLink>
           </li>
           <li class="nav-item">
             <RouterLink to="/main"
-                        class="bi bi-card-list m-2 fs-3 icon-link-hover"
+                        :class="['bi bi-card-list m-2 fs-3 icon-link-hover', themeStore.theme === 'light' ? 'text-dark' : 'text-white']"
                         title="Main">
             </RouterLink>
           </li>
           <li class="nav-item">
             <a @click="logout"
-               class="bi bi-box-arrow-right m-2 fs-3 icon-link-hover"
+               :class="['bi bi-box-arrow-right m-2 fs-3 icon-link-hover', themeStore.theme === 'light' ? 'text-dark' : 'text-white']"
                title="Logout"
                style="cursor: pointer;"></a>
           </li>
           <li class="nav-item ms-5 d-flex align-items-center">
-            <i :class="themeStore.theme === 'light' ? 'bi bi-sun fs-3' : 'bi bi-moon-stars fs-3'"></i>
-            <i :class="themeStore.theme === 'light' ? 'bi bi-toggle-off fs-3 mx-2' : 'bi bi-toggle-on fs-3 mx-2'"
+            <i :class="[themeStore.theme === 'light' ? 'bi bi-sun fs-3 text-dark' : 'bi bi-moon-stars fs-3 text-white']"></i>
+            <i :class="[themeStore.theme === 'light' ? 'bi bi-toggle-off fs-3 mx-2 text-dark' : 'bi bi-toggle-on fs-3 mx-2 text-white']"
                @click="toggleTheme"></i>
           </li>
         </ul>
