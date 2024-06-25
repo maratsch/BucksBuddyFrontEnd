9d16f0a71322507387fe9fcc93c600521cfc7ea8
Norman <peters.norman91@gmail.com>
2024-06-25 20:42:22 +0200

    switch to render

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,8 +4,8 @@ import axios from 'axios';
 import type {Expenditure, Journey, newUser} from '@/types';
 
 const apiClient = axios.create({
-    baseURL: //'https://bucksbuddybackend.onrender.com/',
-        'http://localhost:8080/',
+    baseURL: 'https://bucksbuddybackend.onrender.com/',
+        //'http://localhost:8080/',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
